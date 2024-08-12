aa3f552e699b775f1b27c226b1fff7e8ea119f15
juanxan <juanxan123@gmail.com>
2024-08-11 20:27:41 -0500

    interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authorizationNodeInterceptor } from './shared/interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authorizationNodeInterceptor])),
     provideClientHydration(),
     provideAnimationsAsync(),
   ],
