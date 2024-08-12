40194987cf73c47251705d763fb1e0b7332a9d3e
juanxan <juanxan123@gmail.com>
2024-08-12 10:15:29 -0500

    post reservation service

--- src/app/modules/reservation/services/reservation.service.ts
+++ src/app/modules/reservation/services/reservation.service.ts
@@ -4,7 +4,7 @@ import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { IGetCustomer, IGetServices } from '../interfaces';
 import { IBasicDbResponse } from '../../../shared/models';
-import { IGetReservation } from '../interfaces/reservation.interface';
+import { IGetReservation, IPostReservation } from '../interfaces/reservation.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +22,7 @@ export class ReservationService {
    * Método que trae todos los clientes de DB
    * @returns {IBasicDbResponse<IGetCustomer>}
    */
-  getCustomer(): Observable<IBasicDbResponse<IGetCustomer[]>>{
+  getCustomer(): Observable<IBasicDbResponse<IGetCustomer[]>> {
     const url = `${this.urlBack}/api/Reservations/getCustomers`;
     return this._http.get<IBasicDbResponse<IGetCustomer[]>>(url);
   }
@@ -33,7 +33,7 @@ export class ReservationService {
    * Método que trae todos los servicios de DB
    * @returns {IBasicDbResponse<IGetCustomer>}
    */
-  getService(): Observable<IBasicDbResponse<IGetServices[]>>{
+  getService(): Observable<IBasicDbResponse<IGetServices[]>> {
     const url = `${this.urlBack}/api/Reservations/getServices`;
     return this._http.get<IBasicDbResponse<IGetServices[]>>(url);
   }
@@ -45,10 +45,21 @@ export class ReservationService {
    * @param { HttpParams } param
    * @returns {IBasicDbResponse<IGetCustomer>} return IGetCustomer
    */
-  getReservations(param: HttpParams): Observable<IBasicDbResponse<IGetReservation[]>>{
+  getReservations(param: HttpParams): Observable<IBasicDbResponse<IGetReservation[]>> {
     const url = `${this.urlBack}/api/Reservations/getReservations`;
-    return this._http.get<IBasicDbResponse<IGetReservation[]>>(url, {params: param});
+    return this._http.get<IBasicDbResponse<IGetReservation[]>>(url, { params: param });
   }
 
+  /**
+   * Método https
+   *
+   * Creacion reservacion
+   * @param {IPostReservation} data
+   * @returns {Observable} return
+   */
+  postReservation(data: IPostReservation) {
+    const url = `${this.urlBack}/api/Reservations/createReservation`;
+    return this._http.post(url, data);
+  }
   //#endregion
 }
